6e156a13f2a0ca51dcba77f105c01861ce17c515
Yadunand <yadunut@gmail.com>
2017-11-05 17:21:30 +0800

    Did the redux stuff for getting line status

--- src/redux/action.creators.ts
+++ src/redux/action.creators.ts
@@ -1,3 +1,47 @@
-import * as constants from "./action.constants";
+import * as Constants from "./action.constants";
+import _axios from "axios";
+import apiConstants from "../config/index";
 
-export default {};
+const axios = _axios.create({
+  baseURL: apiConstants.api.baseUrl,
+});
+
+function fetchLineStatusSuccess(data: any) {
+  return {
+    type: Constants.FETCH_LINE_STATUS_SUCCESS,
+    isFetching: false,
+    data,
+  };
+}
+
+function fetchLineStatusFailure(error: any) {
+  return {
+    type: Constants.FETCH_LINE_STATUS_FAILURE,
+    isFetching: false,
+    error,
+  };
+}
+
+function fetchingLineStatus() {
+  return {
+    type: Constants.FETCH_LINE_STATUS,
+    isFetching: true,
+  };
+}
+
+function fetchLineStatus() {
+  return (dispatch: any) => {
+    dispatch(fetchingLineStatus());
+    axios.get("/status").then(
+      (response) => dispatch(fetchLineStatusSuccess(response.data)),
+      (error) => dispatch(fetchLineStatusFailure(error)),
+    );
+  };
+}
+
+export {
+  fetchLineStatus,
+  fetchingLineStatus,
+  fetchLineStatusSuccess,
+  fetchLineStatusFailure,
+};
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,13 +1,44 @@
-import * as constants from "./action.constants";
+import * as Constants from "./action.constants";
 import { combineReducers } from "redux";
 // Import reducers
 
+/*
+const mockData = {
+  "description": "EWL is down, sian.",
+  "lines": {
+    "EWL": {
+      ”short_name": "EWL",
+      "level": 4,
+      "timestamp": <<date in ISO8601>>,
+    },
+    "NSL": {
+      "short_name": "NSL",
+      "level": 2,
+      "timestamp": <<date in ISO8601>>,
+    },
+  }
+}
+*/
+
 const initialState = {
-  state: false,
-  description: "Woah some Text",
+  status: {
+    description: "",
+    lines: {},
+    isFetching: false,
+    tweets: {},
+  },
 };
 const reducer = (state = initialState, action: any) => {
-  return state;
+  switch (action.type) {
+    case Constants.FETCH_LINE_STATUS:
+      return { ...state, isFetching: action.isFetching };
+    case Constants.FETCH_LINE_STATUS_SUCCESS:
+      return { ...state, isFetching: action.isFetching, lines: action.data };
+    case Constants.FETCH_LINE_STATUS_FAILURE:
+      return { ...state, isFetching: action.isFetching };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
